20af6b6a4b581351af20310132f01e63fc328fa7
youba idjis <youba.etud@gmail.com>
2023-02-11 23:29:33 -0800

    RestFul API

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,48 @@ class MoviesController < ApplicationController
         movies = Movie.all 
         render json: movies
     end
+
+    def create
+        movie = Movie.create(movie_params)
+        if movie.valid?
+            render json: { movie: movie}, status: :created
+        else
+            render json: {error: "Failed to add movie"}
+        end
+    end
+
+    def show
+        movie = Movie.find_by(id: params[:id])
+        if movie 
+            render json: movie
+        else
+            render json: {error: "Failed to load movie"}
+        end
+    end
+
+    def update
+        movie = Movie.find_by(id: params[:id])
+        if movie.valid?
+            movie.update(movie_params)
+            render json: movie
+        else
+            render json: {error: "movie does not exist"}, status: :not_found
+        end
+    end
+
+    def destroy
+        movie = Movie.find_by(id: params[:id])
+        if movie
+            movie.destroy
+            render json: {msg: 'movie deleted'}
+        else
+            render json: {error: 'movie does not exist'}, status: :not_found
+        end
+    end
+
+    private 
+
+    def movie_params
+        params.require(:movie).permit(:title, :year, :length, :description, :poster_url, :category, :discount, :female_director)
+    end
 end
